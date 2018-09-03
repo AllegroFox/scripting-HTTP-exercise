64837bcdfc7a938bc1617cd96b5787db5ca53662
Emily McMinn <emily@mcminn.ca>
2018-09-03 15:34:14 +0000

    Console logging data in chunks.

--- step_1/getAndPrintHTMLChunks.js
+++ step_1/getAndPrintHTMLChunks.js
@@ -14,6 +14,7 @@ function getAndPrintHTMLChunks () {
 
     response.on('data', function(data) {
       console.log('Data received. Chunk size: ', data.length);
+      console.log(data, 'n/');
     });
 
     response.on('end', function() {
