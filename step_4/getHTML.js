5df979d392a545e81ecdecca7baae985cbe2e266
Emily McMinn <emily@mcminn.ca>
2018-09-03 16:46:55 +0000

    added callbacks

---
+++ step_4/getHTML.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+
+function getHTML (options, callback) {
+
+  var options = {
+    host: process.argv[2],
+    path: process.argv[3]
+  };
+
+  https.get(options, function(response){
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+
+        printHTML(data);
+
+      });
+
+    response.on('end', function() {
+      console.log('Response ended.');
+    });
+  })
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML();
+
+
