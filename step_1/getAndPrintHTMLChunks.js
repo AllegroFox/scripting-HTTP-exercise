a5aaba86716914661cc90c21ad01190695603915
Emily McMinn <emily@mcminn.ca>
2018-09-03 15:25:22 +0000

    added basic GET function (no response handler yet)

---
+++ step_1/getAndPrintHTMLChunks.js
@@ -0,0 +1,18 @@
+var https = require('https');
+
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response){
+    console.log(response);
+  })
+}
+
+getAndPrintHTMLChunks();
+
+
